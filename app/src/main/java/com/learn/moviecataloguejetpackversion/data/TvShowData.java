df571a0814b5a83ee0ed0ed7a342f6d6b661e763
M Nazar Alwi <alwinazar75@gmail.com>
2019-11-07 14:19:31 +0700

    Add movie and tv show data

---
+++ app/src/main/java/com/learn/moviecataloguejetpackversion/data/TvShowData.java
@@ -0,0 +1,44 @@
+package com.learn.moviecataloguejetpackversion.data;
+
+public class TvShowData {
+    public TvShowData() {
+
+    }
+
+    public static String[] dataTvShowName = new String[] {
+            "Arrow", "Supernatural", "The Flash", "Fear the Walking Dead", "The Simpsons", "See", "Family Guy", "Riverdale", "Grey's Anatomy", "South Park"
+    };
+
+    public static String[] dataTvShowRelease = new String[] {
+            "2012-10-10", "2005-09-13", "2014-10-07", "2015-08-23", "1989-12-17", "2019-11-01" , "1999-01-31", "2017-01-26", "2005-03-27", "1997-08-13"
+    };
+
+    public static String[] dataTvShowVote = new String[] {
+            "5.8", "7.4", "6.7", "6.3", "7.1", "7.9", "6.5", "7.2", "6.4", "7.8"
+    };
+
+    public static String[] dataTvShowPopularity = new String[] {
+            "418.603", "235.731", "225.559", "325.071", "185.702", "172.71", "158.91", "119.77", "128.762", "117.581"
+    };
+
+    public static String[] dataTvShowOverview = new String[] {
+            "Spoiled billionaire playboy Oliver Queen is missing and presumed dead when his yacht is lost at sea. He returns five years later a changed man, determined to clean up the city as a hooded vigilante armed with a bow.",
+            "When they were boys, Sam and Dean Winchester lost their mother to a mysterious and demonic supernatural force. Subsequently, their father raised them to be soldiers. He taught them about the paranormal evil that lives in the dark corners and on the back roads of America ... and he taught them how to kill it. Now, the Winchester brothers crisscross the country in their '67 Chevy Impala, battling every kind of supernatural threat they encounter along the way. ",
+            "After a particle accelerator causes a freak storm, CSI Investigator Barry Allen is struck by lightning and falls into a coma. Months later he awakens with the power of super speed, granting him the ability to move through Central City like an unseen guardian angel. Though initially excited by his newfound powers, Barry is shocked to discover he is not the only \"meta-human\" who was created in the wake of the accelerator explosion -- and not everyone is using their new powers for good. Barry partners with S.T.A.R. Labs and dedicates his life to protect the innocent. For now, only a few close friends and associates know that Barry is literally the fastest man alive, but it won't be long before the world learns what Barry Allen has become...The Flash.",
+            "What did the world look like as it was transforming into the horrifying apocalypse depicted in \"The Walking Dead\"? This spin-off set in Los Angeles, following new characters as they face the beginning of the end of the world, will answer that question.",
+            "Set in Springfield, the average American town, the show focuses on the antics and everyday adventures of the Simpson family; Homer, Marge, Bart, Lisa and Maggie, as well as a virtual cast of thousands. Since the beginning, the series has been a pop culture icon, attracting hundreds of celebrities to guest star. The show has also made name for itself in its fearless satirical take on politics, media and American life in general.",
+            "A virus has decimated humankind. Those who survived emerged blind. Centuries later when twins are born with the mythic ability to see, their father must protect his tribe against a threatened queen.",
+            "Sick, twisted, politically incorrect and Freakin' Sweet animated series featuring the adventures of the dysfunctional Griffin family. Bumbling Peter and long-suffering Lois have three kids. Stewie (a brilliant but sadistic baby bent on killing his mother and taking over the world), Meg (the oldest, and is the most unpopular girl in town) and Chris (the middle kid, he's not very bright but has a passion for movies). The final member of the family is Brian - a talking dog and much more than a pet, he keeps Stewie in check whilst sipping Martinis and sorting through his own life issues.",
+            "Set in the present, the series offers a bold, subversive take on Archie, Betty, Veronica and their friends, exploring the surreality of small-town life, the darkness and weirdness bubbling beneath Riverdale’s wholesome facade.",
+            "Follows the personal and professional lives of a group of doctors at Seattle’s Grey Sloan Memorial Hospital.",
+            "Follows the misadventures of four irreverent grade-schoolers in the quiet, dysfunctional town of South Park, Colorado."
+    };
+
+    public static String[] dataTvShowPhoto = new String[] {
+            "/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg", "/KoYWXbnYuS3b0GyQPkbuexlVK9.jpg", "/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg", "/lZMb3R3e5vqukPbeDMeyYGf2ZNG.jpg", "/yTZQkSsxUFJZJe67IenRM0AEklc.jpg", "/g3JsScc7mQCfc3e5e5rXwu7xVVP.jpg", "/gBGUL1UTUNmdRQT8gA1LUV4yg39.jpg", "/4X7o1ssOEvp4BFLim1AZmPNcYbU.jpg", "/jnsvc7gCKocXnrTXF6p03cICTWb.jpg", "/v9zc0cZpy5aPSfAy6Tgb6I1zWgV.jpg"
+    };
+
+    public static String[] dataTvShowBackdrop = new String[] {
+            "/dXTyVDTIgeByvUOUEiHjbi8xX9A.jpg", "/o9OKe3M06QMLOzTl3l6GStYtnE9.jpg", "/6ZdQTBy20HzWudZthAV7NkZWfIb.jpg", "/7r4FieFH6Eh6S55hi8c9LOiFENg.jpg", "/f5uNbUC76oowt5mt5J9QlqrIYQ6.jpg", "/8TOkxONO3TEeJRuZWb0hG7SboyV.jpg", "/pH38r4TWTqq7Mcs6XAlwgzNUeJe.jpg", "/2IUpoKSP64r6rp2vBo0Fdk8a1UU.jpg", "/edmk8xjGBsYVIf4QtLY9WMaMcXZ.jpg", "/mSDKNVvDfitFE6Fb6fSSl5DQmgS.jpg"
+    };
+}
