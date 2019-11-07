60e3cb13ec2ab87650bbe1c4163f0489b4477dba
M Nazar Alwi <alwinazar75@gmail.com>
2019-11-07 14:25:24 +0700

    Add fragment movie and tv show. Show data using recycler view, and add item_row for item recycler view

---
+++ app/src/main/java/com/learn/moviecataloguejetpackversion/fragment/MoviesFragment.java
@@ -0,0 +1,67 @@
+package com.learn.moviecataloguejetpackversion.fragment;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.learn.moviecataloguejetpackversion.R;
+import com.learn.moviecataloguejetpackversion.activity.DetailMovieActivity;
+import com.learn.moviecataloguejetpackversion.adapter.MovieAdapter;
+import com.learn.moviecataloguejetpackversion.model.MainViewModel;
+import com.learn.moviecataloguejetpackversion.model.Movie;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MoviesFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private ArrayList<Movie> movies = new ArrayList<>();
+
+    public MoviesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movies, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        recyclerView = view.findViewById(R.id.rv_movies);
+        recyclerView.setHasFixedSize(true);
+        movies.addAll(MainViewModel.getListMovie());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        MovieAdapter movieAdapter = new MovieAdapter(movies);
+        recyclerView.setAdapter(movieAdapter);
+
+        movieAdapter.setOnItemClickCallback(new MovieAdapter.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(Movie movie, int position) {
+                Intent goToDetailMovie = new Intent(getActivity(), DetailMovieActivity.class);
+                goToDetailMovie.putExtra(DetailMovieActivity.EXTRA_MOVIES, movies.get(position));
+                startActivity(goToDetailMovie);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/learn/moviecataloguejetpackversion/fragment/TvShowsFragment.java
@@ -0,0 +1,67 @@
+package com.learn.moviecataloguejetpackversion.fragment;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.learn.moviecataloguejetpackversion.R;
+import com.learn.moviecataloguejetpackversion.activity.DetailTvShowActivity;
+import com.learn.moviecataloguejetpackversion.adapter.TvShowAdapter;
+import com.learn.moviecataloguejetpackversion.model.MainViewModel;
+import com.learn.moviecataloguejetpackversion.model.TvShow;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TvShowsFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private ArrayList<TvShow> tvShows = new ArrayList<>();
+
+    public TvShowsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tv_shows, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        recyclerView = view.findViewById(R.id.rv_tv_shows);
+        recyclerView.setHasFixedSize(true);
+        tvShows.addAll(MainViewModel.getListTvShow());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        TvShowAdapter tvShowAdapter = new TvShowAdapter(tvShows);
+        recyclerView.setAdapter(tvShowAdapter);
+
+        tvShowAdapter.setOnItemClickCallback(new TvShowAdapter.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(TvShow tvShow, int position) {
+                Intent goToDetail = new Intent(getActivity(), DetailTvShowActivity.class);
+                goToDetail.putExtra(DetailTvShowActivity.EXTRA_TV_SHOWS, tvShows.get(position));
+                startActivity(goToDetail);
+            }
+        });
+    }
+}
