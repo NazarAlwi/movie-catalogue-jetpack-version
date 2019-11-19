76457e340e3e26b65f26f23468932e3097926870
M Nazar Alwi <alwinazar75@gmail.com>
2019-11-19 21:00:31 +0700

    Refactor view model, create new view model class

---
+++ app/src/main/java/com/learn/moviecataloguejetpackversion/model/MovieDetailViewModel.java
@@ -0,0 +1,29 @@
+package com.learn.moviecataloguejetpackversion.model;
+
+import androidx.lifecycle.ViewModel;
+
+import com.learn.moviecataloguejetpackversion.data.MovieData;
+
+public class MovieDetailViewModel extends ViewModel {
+    private Movie mMovie;
+    private String movieName;
+
+    public Movie getMovieDetail() {
+        for (int i = 0; i < MovieData.generateMovieList().size(); i++) {
+            Movie movie = MovieData.generateMovieList().get(i);
+            if (movie.getNameMovie().equals(movieName)) {
+                mMovie = movie;
+            }
+        }
+
+        return mMovie;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+}
