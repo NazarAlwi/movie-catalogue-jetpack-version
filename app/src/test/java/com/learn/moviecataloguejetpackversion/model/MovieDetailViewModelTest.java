166553411c4aabc8f75c9f43add33dc700affd31
M Nazar Alwi <alwinazar75@gmail.com>
2019-11-19 22:55:26 +0700

    Create test detail view model

--- app/src/test/java/com/learn/moviecataloguejetpackversion/model/MovieDetailViewModelTest.java
+++ app/src/test/java/com/learn/moviecataloguejetpackversion/model/MovieDetailViewModelTest.java
@@ -1,5 +1,34 @@
+package com.learn.moviecataloguejetpackversion.model;
+
+import android.media.midi.MidiOutputPort;
+import android.view.View;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MovieDetailViewModelTest {
+    private MovieDetailViewModel movieDetailViewModel;
+    private Movie dummyMovie;
 
+    @Before
+    public void setUp() {
+        movieDetailViewModel = new MovieDetailViewModel();
+        dummyMovie = new Movie("/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg", "Joker", "During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.", "8.5", "2019-10-04", "534.771", "/n6bUvigpRFqSwmPp1m2YADdbRBc.jpg");
+    }
+
+    @Test
+    public void getMovieDetailTest() {
+        movieDetailViewModel.setMovieName(dummyMovie.getNameMovie());
+        Movie movie = movieDetailViewModel.getMovieDetail();
+        assertNotNull(movie);
+        assertEquals(dummyMovie.getPhotoMovie(), movie.getPhotoMovie());
+        assertEquals(dummyMovie.getNameMovie(), movie.getNameMovie());
+        assertEquals(dummyMovie.getOverviewMovie(), movie.getOverviewMovie());
+        assertEquals(dummyMovie.getVoteMovie(), movie.getVoteMovie());
+        assertEquals(dummyMovie.getReleaseMovie(), movie.getReleaseMovie());
+        assertEquals(dummyMovie.getPopularityMovie(), movie.getPopularityMovie());
+        assertEquals(dummyMovie.getBackdropMovie(), movie.getBackdropMovie());
+    }
 }
--- app/src/test/java/com/learn/moviecataloguejetpackversion/model/TvShowDetailViewModelTest.java
+++ app/src/test/java/com/learn/moviecataloguejetpackversion/model/TvShowDetailViewModelTest.java
@@ -1,5 +1,31 @@
+package com.learn.moviecataloguejetpackversion.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class TvShowDetailViewModelTest {
+    private TvShowDetailViewModel tvShowDetailViewModel;
+    private TvShow dummyTvShow;
 
+    @Before
+    public void setUp() {
+        tvShowDetailViewModel = new TvShowDetailViewModel();
+        dummyTvShow = new TvShow("/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg", "Arrow", "Spoiled billionaire playboy Oliver Queen is missing and presumed dead when his yacht is lost at sea. He returns five years later a changed man, determined to clean up the city as a hooded vigilante armed with a bow.", "5.8", "2012-10-10", "418.603", "/dXTyVDTIgeByvUOUEiHjbi8xX9A.jpg");
+    }
+
+    @Test
+    public void getTvShowDetailTest() {
+        tvShowDetailViewModel.setTvShowName(dummyTvShow.getNameTvShow());
+        TvShow tvShow = tvShowDetailViewModel.getTvShowDetail();
+        assertNotNull(tvShow);
+        assertEquals(dummyTvShow.getPhotoTvShow(), tvShow.getPhotoTvShow());
+        assertEquals(dummyTvShow.getNameTvShow(), tvShow.getNameTvShow());
+        assertEquals(dummyTvShow.getOverviewTvShow(), tvShow.getOverviewTvShow());
+        assertEquals(dummyTvShow.getVoteTvShow(), tvShow.getVoteTvShow());
+        assertEquals(dummyTvShow.getReleaseTvShow(), tvShow.getReleaseTvShow());
+        assertEquals(dummyTvShow.getPopularityTvShow(), tvShow.getPopularityTvShow());
+        assertEquals(dummyTvShow.getBackdropTvShow(), tvShow.getBackdropTvShow());
+    }
 }
