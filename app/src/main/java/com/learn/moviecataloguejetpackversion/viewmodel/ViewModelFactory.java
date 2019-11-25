371411f9a05457fe5118bc71748201a0e72122cd
M Nazar Alwi <alwinazar75@gmail.com>
2019-11-25 21:02:21 +0700

    Create ViewModelFactory

--- app/src/main/java/com/learn/moviecataloguejetpackversion/viewmodel/ViewModelFactory.java
+++ app/src/main/java/com/learn/moviecataloguejetpackversion/viewmodel/ViewModelFactory.java
@@ -1,4 +1,48 @@
 package com.learn.moviecataloguejetpackversion.viewmodel;
 
-public class ViewModelFactory {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.learn.moviecataloguejetpackversion.data.source.MovieCatalogueRepository;
+import com.learn.moviecataloguejetpackversion.di.Injection;
+import com.learn.moviecataloguejetpackversion.model.MainViewModel;
+import com.learn.moviecataloguejetpackversion.model.MovieDetailViewModel;
+import com.learn.moviecataloguejetpackversion.model.TvShowDetailViewModel;
+
+public class ViewModelFactory extends ViewModelProvider.NewInstanceFactory {
+    private static volatile ViewModelFactory INSTANCE;
+
+    private final MovieCatalogueRepository mMovieCatalogueRepository;
+
+    public ViewModelFactory(MovieCatalogueRepository MovieCatalogueRepository) {
+        this.mMovieCatalogueRepository = MovieCatalogueRepository;
+    }
+
+    public static ViewModelFactory getINSTANCE(Application application) {
+        if (INSTANCE == null) {
+            synchronized (ViewModelFactory.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new ViewModelFactory(Injection.providerRepository(application));
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(MainViewModel.class)) {
+            return (T) new MainViewModel(mMovieCatalogueRepository);
+        } else if (modelClass.isAssignableFrom(MovieDetailViewModel.class)) {
+            return (T) new MovieDetailViewModel(mMovieCatalogueRepository);
+        } else if (modelClass.isAssignableFrom(TvShowDetailViewModel.class)) {
+            return (T) new TvShowDetailViewModel(mMovieCatalogueRepository);
+        }
+
+        throw new IllegalArgumentException("Unknown ViewModel class: " + modelClass.getName());
+    }
 }
