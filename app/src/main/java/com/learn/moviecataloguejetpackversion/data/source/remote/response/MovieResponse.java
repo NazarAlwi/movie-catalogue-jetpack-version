96edd1e251e1e94a3c92aed8824fb862578fb5e5
M Nazar Alwi <alwinazar75@gmail.com>
2019-11-25 19:58:17 +0700

    Add response class

---
+++ app/src/main/java/com/learn/moviecataloguejetpackversion/data/source/remote/response/MovieResponse.java
@@ -0,0 +1,118 @@
+package com.learn.moviecataloguejetpackversion.data.source.remote.response;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MovieResponse implements Parcelable {
+    private String photoMovie;
+    private String nameMovie;
+    private String overviewMovie;
+    private String voteMovie;
+    private String releaseMovie;
+    private String popularityMovie;
+    private String backdropMovie;
+
+    public MovieResponse(String photoMovie, String nameMovie, String overviewMovie, String voteMovie, String releaseMovie, String popularityMovie, String backdropMovie) {
+        this.photoMovie = photoMovie;
+        this.nameMovie = nameMovie;
+        this.overviewMovie = overviewMovie;
+        this.voteMovie = voteMovie;
+        this.releaseMovie = releaseMovie;
+        this.popularityMovie = popularityMovie;
+        this.backdropMovie = backdropMovie;
+    }
+
+    public String getPhotoMovie() {
+        return photoMovie;
+    }
+
+    public void setPhotoMovie(String photoMovie) {
+        this.photoMovie = photoMovie;
+    }
+
+    public String getNameMovie() {
+        return nameMovie;
+    }
+
+    public void setNameMovie(String nameMovie) {
+        this.nameMovie = nameMovie;
+    }
+
+    public String getOverviewMovie() {
+        return overviewMovie;
+    }
+
+    public void setOverviewMovie(String overviewMovie) {
+        this.overviewMovie = overviewMovie;
+    }
+
+    public String getVoteMovie() {
+        return voteMovie;
+    }
+
+    public void setVoteMovie(String voteMovie) {
+        this.voteMovie = voteMovie;
+    }
+
+    public String getReleaseMovie() {
+        return releaseMovie;
+    }
+
+    public void setReleaseMovie(String releaseMovie) {
+        this.releaseMovie = releaseMovie;
+    }
+
+    public String getPopularityMovie() {
+        return popularityMovie;
+    }
+
+    public void setPopularityMovie(String popularityMovie) {
+        this.popularityMovie = popularityMovie;
+    }
+
+    public String getBackdropMovie() {
+        return backdropMovie;
+    }
+
+    public void setBackdropMovie(String backdropMovie) {
+        this.backdropMovie = backdropMovie;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(photoMovie);
+        parcel.writeString(nameMovie);
+        parcel.writeString(overviewMovie);
+        parcel.writeString(voteMovie);
+        parcel.writeString(releaseMovie);
+        parcel.writeString(popularityMovie);
+        parcel.writeString(backdropMovie);
+    }
+
+    protected MovieResponse(Parcel in) {
+        photoMovie = in.readString();
+        nameMovie = in.readString();
+        overviewMovie = in.readString();
+        voteMovie = in.readString();
+        releaseMovie = in.readString();
+        popularityMovie = in.readString();
+        backdropMovie = in.readString();
+    }
+
+    public static final Creator<MovieResponse> CREATOR = new Creator<MovieResponse>() {
+        @Override
+        public MovieResponse createFromParcel(Parcel in) {
+            return new MovieResponse(in);
+        }
+
+        @Override
+        public MovieResponse[] newArray(int size) {
+            return new MovieResponse[size];
+        }
+    };
+}
---
+++ app/src/main/java/com/learn/moviecataloguejetpackversion/data/source/remote/response/TvShowResponse.java
@@ -0,0 +1,118 @@
+package com.learn.moviecataloguejetpackversion.data.source.remote.response;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class TvShowResponse implements Parcelable {
+    private String photoTvShow;
+    private String nameTvShow;
+    private String overviewTvShow;
+    private String voteTvShow;
+    private String releaseTvShow;
+    private String popularityTvShow;
+    private String backdropTvShow;
+
+    public TvShowResponse(String photoTvShow, String nameTvShow, String overviewTvShow, String voteTvShow, String releaseTvShow, String popularityTvShow, String backdropTvShow) {
+        this.photoTvShow = photoTvShow;
+        this.nameTvShow = nameTvShow;
+        this.overviewTvShow = overviewTvShow;
+        this.voteTvShow = voteTvShow;
+        this.releaseTvShow = releaseTvShow;
+        this.popularityTvShow = popularityTvShow;
+        this.backdropTvShow = backdropTvShow;
+    }
+
+    public String getPhotoTvShow() {
+        return photoTvShow;
+    }
+
+    public void setPhotoTvShow(String photoTvShow) {
+        this.photoTvShow = photoTvShow;
+    }
+
+    public String getNameTvShow() {
+        return nameTvShow;
+    }
+
+    public void setNameTvShow(String nameTvShow) {
+        this.nameTvShow = nameTvShow;
+    }
+
+    public String getOverviewTvShow() {
+        return overviewTvShow;
+    }
+
+    public void setOverviewTvShow(String overviewTvShow) {
+        this.overviewTvShow = overviewTvShow;
+    }
+
+    public String getVoteTvShow() {
+        return voteTvShow;
+    }
+
+    public void setVoteTvShow(String voteTvShow) {
+        this.voteTvShow = voteTvShow;
+    }
+
+    public String getReleaseTvShow() {
+        return releaseTvShow;
+    }
+
+    public void setReleaseTvShow(String releaseTvShow) {
+        this.releaseTvShow = releaseTvShow;
+    }
+
+    public String getPopularityTvShow() {
+        return popularityTvShow;
+    }
+
+    public void setPopularityTvShow(String popularityTvShow) {
+        this.popularityTvShow = popularityTvShow;
+    }
+
+    public String getBackdropTvShow() {
+        return backdropTvShow;
+    }
+
+    public void setBackdropTvShow(String backdropTvShow) {
+        this.backdropTvShow = backdropTvShow;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(photoTvShow);
+        parcel.writeString(nameTvShow);
+        parcel.writeString(overviewTvShow);
+        parcel.writeString(voteTvShow);
+        parcel.writeString(releaseTvShow);
+        parcel.writeString(popularityTvShow);
+        parcel.writeString(backdropTvShow);
+    }
+
+    protected TvShowResponse(Parcel in) {
+        photoTvShow = in.readString();
+        nameTvShow = in.readString();
+        overviewTvShow = in.readString();
+        voteTvShow = in.readString();
+        releaseTvShow = in.readString();
+        popularityTvShow = in.readString();
+        backdropTvShow = in.readString();
+    }
+
+    public static final Creator<TvShowResponse> CREATOR = new Creator<TvShowResponse>() {
+        @Override
+        public TvShowResponse createFromParcel(Parcel in) {
+            return new TvShowResponse(in);
+        }
+
+        @Override
+        public TvShowResponse[] newArray(int size) {
+            return new TvShowResponse[size];
+        }
+    };
+}
