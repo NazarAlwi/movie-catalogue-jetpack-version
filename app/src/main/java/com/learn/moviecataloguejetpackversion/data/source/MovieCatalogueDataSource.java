d611a4796ffa300090c65605f2536eaa0f2f28bf
M Nazar Alwi <alwinazar75@gmail.com>
2019-11-27 22:42:18 +0700

    Add data source

---
+++ app/src/main/java/com/learn/moviecataloguejetpackversion/data/source/MovieCatalogueDataSource.java
@@ -0,0 +1,13 @@
+package com.learn.moviecataloguejetpackversion.data.source;
+
+import com.learn.moviecataloguejetpackversion.data.source.local.entity.Movie;
+import com.learn.moviecataloguejetpackversion.data.source.local.entity.TvShow;
+
+import java.util.List;
+
+public interface MovieCatalogueDataSource {
+    List<Movie> getAllMovie();
+    List<TvShow> getAllTvShow();
+    Movie getMovieById(String idMovie);
+    TvShow getTvShow(String idTvShow);
+}
