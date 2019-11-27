911a6f622ba1600634481be62070b08481ca07b3
M Nazar Alwi <alwinazar75@gmail.com>
2019-11-27 22:44:08 +0700

    Add injection

---
+++ app/src/main/java/com/learn/moviecataloguejetpackversion/di/Injection.java
@@ -0,0 +1,16 @@
+package com.learn.moviecataloguejetpackversion.di;
+
+import android.app.Application;
+
+import com.learn.moviecataloguejetpackversion.data.source.MovieCatalogueRepository;
+import com.learn.moviecataloguejetpackversion.data.source.remote.RemoteRepository;
+import com.learn.moviecataloguejetpackversion.utils.JsonHelper;
+
+public class Injection {
+    public static MovieCatalogueRepository provideRepository(Application application) {
+
+        RemoteRepository remoteRepository = RemoteRepository.getInstance(new JsonHelper(application));
+
+        return MovieCatalogueRepository.getInstance(remoteRepository);
+    }
+}
