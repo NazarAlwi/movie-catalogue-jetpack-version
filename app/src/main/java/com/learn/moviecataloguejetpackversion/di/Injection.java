7648204a0e78de148253db6b910bd1d44bad45c4
M Nazar Alwi <alwinazar75@gmail.com>
2019-12-03 10:19:38 +0700

    Modify injection

--- app/src/main/java/com/learn/moviecataloguejetpackversion/di/Injection.java
+++ app/src/main/java/com/learn/moviecataloguejetpackversion/di/Injection.java
@@ -3,14 +3,20 @@ package com.learn.moviecataloguejetpackversion.di;
 import android.app.Application;
 
 import com.learn.moviecataloguejetpackversion.data.source.MovieCatalogueRepository;
+import com.learn.moviecataloguejetpackversion.data.source.local.LocalRepository;
+import com.learn.moviecataloguejetpackversion.data.source.local.room.MovieCatalogueDatabase;
 import com.learn.moviecataloguejetpackversion.data.source.remote.RemoteRepository;
+import com.learn.moviecataloguejetpackversion.utils.AppExecutors;
 import com.learn.moviecataloguejetpackversion.utils.JsonHelper;
 
 public class Injection {
     public static MovieCatalogueRepository provideRepository(Application application) {
+        MovieCatalogueDatabase movieCatalogueDatabase = MovieCatalogueDatabase.getInstance(application);
 
+        LocalRepository localRepository = LocalRepository.getInstance(movieCatalogueDatabase.movieCatalogueDao());
         RemoteRepository remoteRepository = RemoteRepository.getInstance(new JsonHelper(application));
+        AppExecutors appExecutors = new AppExecutors();
 
-        return MovieCatalogueRepository.getInstance(remoteRepository);
+        return MovieCatalogueRepository.getInstance(localRepository, remoteRepository, appExecutors);
     }
 }
